fc05b5033eb924c32ddb616cb224173f9ba67231
moveslikeH <haoyh19@mails.tsinghua.edu.cn>
2024-01-07 17:41:03 +0800

    add video

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Wide-field Wavefront Sensor (WWS)
 
-<video src="E:\Project\Widefield_wavefront_sensor\docs\test_short_720p.mp4"></video>
+<video src=".\docs\test_short_720p.mp4"></video>
--- src/phase_prediction_model/data_utils.py
+++ src/phase_prediction_model/data_utils.py
@@ -11,7 +11,38 @@ class ZernikeDataset(Dataset):
     norm = None   # scalar
     Z2P, Z2p, p2Z = None, None, None
 
-    
+    def __init__(self, device, train=True, **kwargs):
+        super().__init__()
+
+        self.device = device
+        self.train = train
+        self.kwargs = kwargs
+
+        self.data_path = self.kwargs["dataset"]["data_path"]
+        self.zernike_phase_path = self.kwargs["dataset"]["data_param_path"]
+
+        self.phase_size = self.kwargs["dataset"]["phase_size"]
+        self.n_channel = list(range(self.kwargs["dataset"]["n_channel"][0], self.kwargs["dataset"]["n_channel"][1]+1))
+
+        self.sets = self.kwargs["dataset"]["sets"]
+        self.split = self.kwargs["dataset"]["split"]
+
+        self.t_series = self.kwargs["dataset"]["t_series"]
+        self.t_offset = self.kwargs["dataset"]["t_offset"]
+        self.t_down = self.kwargs["dataset"]["t_down"]
+        self.all_patch = self.kwargs["dataset"]["all_patch"]
+        self.input_patch = self.kwargs["dataset"]["input_patch"]
+        self.target_patch = self.kwargs["dataset"]["target_patch"]
+        self.test_size = self.kwargs["dataset"]["test_size"]
+
+        self.seq_len = (self.t_series + self.t_offset) + (self.t_series + self.t_offset - 1) * self.t_down
+
+        if ZernikeDataset.norm is None:
+            ZernikeDataset.load_zernike_phase(self.device, self.data_param_path, self.phase_size, self.n_channel)
+            ZernikeDataset.calc_norm_zernike(self.device, self.norm_sets, self.data_path, self.data_type, self.mean_type, self.norm_type, self.n_channel)
+            ZernikeDataset.update(self.device, self.data_path, self.mean_type, 
+                                  self.train_sets, self.train_metas, self.test_sets, self.test_metas, 
+                                  self.norm_sets, self.n_channel, self.seq_len)
 
 
 
